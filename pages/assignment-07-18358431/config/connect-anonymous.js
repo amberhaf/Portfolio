0c664e25ef6687bfcb87bb7d384876e96dc39627
amberhaf <amberhaf@hotmail.com>
2020-08-09 12:35:51 +0100

    cleanedup

--- pages/assignment-07-18358431/config/connect-anonymous.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    database: {
-        url: "mongodb://<user>:<pass>@localhost:27017/test"
-    }
-}
--- pages/assignment-07-18358431/config/connect.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    database: {
-        url: "mongodb+srv://User1:UbMN1@cluster0-jmugt.mongodb.net/test?retryWrites=true&w=majority"
-    }
-}
